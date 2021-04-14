0069c7f40032011abffa879515f47933a59851e1
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-04-14 19:56:05 +0200

    Update deploy-with-xcopy-and-yarn.cmd

--- deploy-with-xcopy-and-yarn.cmd
+++ deploy-with-xcopy-and-yarn.cmd
@@ -1,5 +1,7 @@
-xcopy %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET% /S /H /Y
+echo >>>> installing yarn
 npm i -g yarn
+echo >>>> xcopying
+xcopy %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET% /S /H /Y
 echo >>>> going to %DEPLOYMENT_TARGET%
 cd %DEPLOYMENT_TARGET%
 echo >>>> doing ls
