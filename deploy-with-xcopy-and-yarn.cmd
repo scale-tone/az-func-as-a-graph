c952c18fbaed9a1b656e21a785622b2e8168bc76
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-04-14 23:08:56 +0200

    more tests

--- deploy-with-xcopy-and-yarn.cmd
+++ deploy-with-xcopy-and-yarn.cmd
@@ -1,4 +1,12 @@
 call npm install yarn -g --silent
-xcopy %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET% /S /H /Y
-cd %DEPLOYMENT_TARGET%
+
+SET MY_BUILD_TEMP_FOLDER=%TMP%\D72793BA373B4EBB80AEC9E6CF1E0C0E
+
+xcopy %DEPLOYMENT_SOURCE% %MY_BUILD_TEMP_FOLDER% /S /H /Y
+cd %MY_BUILD_TEMP_FOLDER%
 yarn
+
+mkdir d:\home\data\SitePackages
+echo package.zip > d:\home\data\SitePackages\packagename.txt
+
+powershell "Compress-Archive %MY_BUILD_TEMP_FOLDER%\* d:\home\data\SitePackages\package.zip"
