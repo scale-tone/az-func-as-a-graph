8e99518d1ff9856e056d1db84a3d8d545f02b896
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-04-14 20:01:31 +0200

    Update deploy-with-xcopy-and-yarn.cmd

--- deploy-with-xcopy-and-yarn.cmd
+++ deploy-with-xcopy-and-yarn.cmd
@@ -1,10 +1,19 @@
-echo kkk installing yarn
-npm i -g yarn
 echo kkk xcopying
+
 xcopy %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET% /S /H /Y
-echo kkk going to %DEPLOYMENT_TARGET%
-cd %DEPLOYMENT_TARGET%
+
 echo kkk doing ls
+
 ls
+
+echo kkk going to %DEPLOYMENT_TARGET%
+
+cd %DEPLOYMENT_TARGET%
+
+echo kkk doing ls
+
+ls
+
 echo kkk doing yarn
+
 yarn
