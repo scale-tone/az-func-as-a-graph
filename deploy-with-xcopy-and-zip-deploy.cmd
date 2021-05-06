8874498e96b95ec2e9a28543e2fbdf655a0ed6de
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-05-06 22:43:30 +0300

    more deployment experiments

---
+++ deploy-with-xcopy-and-zip-deploy.cmd
@@ -0,0 +1,30 @@
+:: ----------------------
+:: Custom Deployment Script
+:: Fetches npm packages inside a temp folder (which is much faster), then zips it and mounts that zip file.
+:: ----------------------
+
+@IF "%WEBSITE_RUN_FROM_PACKAGE%" NEQ "1" (
+    echo For this script to work you need to enable Run from Package, aka set WEBSITE_RUN_FROM_PACKAGE=1 
+    exit /b 1
+)
+
+SET MY_BUILD_TEMP_FOLDER=%TMP%\D72793BA373B4EBB80AEC9E6CF1E0C0E
+
+mkdir %MY_BUILD_TEMP_FOLDER%
+xcopy %DEPLOYMENT_SOURCE% %MY_BUILD_TEMP_FOLDER% /S /H /Y
+IF %errorlevel% NEQ 0 goto end
+
+cd %MY_BUILD_TEMP_FOLDER%
+
+call npm install --production
+IF %errorlevel% NEQ 0 goto end
+
+mkdir d:\home\data\SitePackages
+echo package.zip > d:\home\data\SitePackages\packagename.txt
+del /Q d:\home\data\SitePackages\package.zip
+
+powershell "$ProgressPreference = 'SilentlyContinue'; Compress-Archive %MY_BUILD_TEMP_FOLDER%\* d:\home\data\SitePackages\package.zip"
+
+:end
+rmdir /S /Q %MY_BUILD_TEMP_FOLDER%
+exit /b %ERRORLEVEL%
